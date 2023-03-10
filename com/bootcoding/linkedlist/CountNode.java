9bf2b8a8d6f07e655d89650ab8038c82c3da55e2
bootcoding#123 <ModakAkshali211@gmail.com>
2023-03-10 23:20:01 +0530

    Refactor

--- com/bootcoding/linkedlist/CountNode.java
+++ com/bootcoding/linkedlist/CountNode.java
@@ -1,19 +1,19 @@
-package LinkedListDsa;
+package com.bootcoding.linkedlist;
 
 public class CountNode {
     public static void main(String[] args) {
       Node head = createLinkedList();
       printCountNodeElements(head);
     }
-    static class Node {
-        int data;
-        Node next;
-
-        public Node(int data) {
-            this.data = data;
-            this.next = null;
-        }
-    }
+//    static class Node {
+//        int data;
+//        Node next;
+//
+//        public Node(int data) {
+//            this.data = data;
+//            this.next = null;
+//        }
+//    }
     private static void printCountNodeElements(Node head){
        Node temp = head;
        int count = 0;
